c4a018e029eba42bb8f771634141e88704de2917
Todd Carlson <todd.carlson3030@me.com>
2019-04-26 06:30:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ function nowServing(katzDeli) {
   }
 }
 
-function currentLine() {
-  
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
